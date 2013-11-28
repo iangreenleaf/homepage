93eec3e759fcdd39fff5feab3473c8c9924233fa
Ian Young <ian.greenleaf@gmail.com>
2013-11-28 10:20:06 -0800

    Run tab init on all page navs

--- src/javascripts/application.js
+++ src/javascripts/application.js
@@ -27,10 +27,14 @@ $(function() {
     $("nav li.current").removeClass("current");
     $(options.clickedElement).parent("li").addClass("current");
   }
-  trickOutContent();
-  $("body").bind("pjax:end", trickOutContent);
-  $("body").bind("pjax:start", menuTransition);
-  $("nav li a").pjax("#content", { fragment: ".shim" });
 
-  $("#offerings").simpleTabs({navSelector: "ul.tabs", scrollTo: function() { return 0 }});
+  var pageSetup = function() {
+    trickOutContent();
+    $("#offerings").simpleTabs({navSelector: "ul.tabs", scrollTo: function() { return 0 }});
+  }
+
+  $("body").bind("pjax:start", menuTransition);
+  $("body").bind("pjax:end", pageSetup);
+  pageSetup();
+  $("nav li a").pjax("#content", { fragment: ".shim" });
 });
